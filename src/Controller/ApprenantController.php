59bda498e9c5c1623486bea5b10780f48d4eafd8
as199 <dioneassane0290@gmail.com>
2020-11-18 22:35:50 +0000

    second commit

--- src/Controller/ApprenantController.php
+++ src/Controller/ApprenantController.php
@@ -2,40 +2,41 @@
 
 namespace App\Controller;
 
-use App\Entity\Admin;
+use App\Entity\Apprenant;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
-class AdminController extends AbstractController
+class ApprenantController extends AbstractController
 {
     /**
      * @Route(
-     *     "api/admin/users",
-     *      name="addUser",
+     *     "api/apprenants",
+     *      name="addApprenant",
      *     methods={"POST"},
+     *     @IsGranted("ROLE_ADMIN")
      *     defaults={
-     *      "_api_resource_class"=User::class,
-     *      "_api_collection_operation_name"="addUser"
+     *      "_api_resource_class"=Apprenant::class,
+     *      "_api_collection_operation_name"="addApprenant"
      *     }
      *     )
      */
-    public function AddUser(EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder, Request $request,SerializerInterface $serializer)
+    public function addApprenant(EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder, Request $request,SerializerInterface $serializer)
     {
 
         $userReq = $request->request->all();
         $file = $request->files->get('avartar')->getRealPath();
         $avartar = fopen($file,'r+');
         $userReq['avartar']=$avartar;
-        $newUser = $serializer->denormalize($userReq, Admin::class);
+        $newUser = $serializer->denormalize($userReq, Apprenant::class);
         $newUser->setPassword($encoder->encodePassword($newUser,$userReq['password']));
-        $newUser->setRoles(['ROLE_ADMIN']);
+        $newUser->setRoles(['ROLE_USER']);
+
         $manager->persist($newUser);
         $manager->flush();
         return new JsonResponse("success",200,[],true);
