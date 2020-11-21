3b558ae6443841f002830196e7f8a4385d67a4ef
as199 <dioneassane0290@gmail.com>
2020-11-21 06:42:26 +0000

    save doc

--- src/DataFixtures/GroupesFixtures.php
+++ src/DataFixtures/GroupesFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Competence;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,6 +10,12 @@ class GroupesFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $competences =["SQL","PHP","CSS","JS"];
+        foreach ($competences as $libelle){
+            $competence = new Competence();
+            $competence->setLibelle($libelle);
+            $manager->persist($competence);
+        }
         // $product = new Product();
         // $manager->persist($product);
 
