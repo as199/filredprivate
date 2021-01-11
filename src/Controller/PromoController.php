d8bca47cb915b75547553beaf278dc3e1f256521
ATPRO <dioneassane0290@gmail.com>
2021-01-11 20:20:46 +0000

    upload fichier exel to add promo apprenant

--- src/Controller/PromoController.php
+++ src/Controller/PromoController.php
@@ -55,9 +55,97 @@ class PromoController extends AbstractController
         $this->profilRepository =$profilRepository;
     }
 
-    public function EditPromo()
+    public function EditPromo($id, Request $request,PromoService $promoService): JsonResponse
     {
-       dd("salut");
+
+        $promoreq = $request->request->all();
+
+        $myPromo = $this->promoReposity->find($id);
+        //dd($myPromo);
+        foreach($myPromo->getApprenants() as  $key=> $value ){
+            //dd($value);
+            $myPromo->removeApprenant($value);
+        }
+
+        if( $request->files->get('upload')){
+
+            $file= $request->files->get('upload');
+            $user = $promoService->uploadExcel($file);
+            $users = $promoService->toArray($user);
+            for ($p =0; $p < count($users); $p++){
+                if ($users[$p]['email']){
+                    if( $this->apprenantReposity->findOneBy(['email'=>$users[$p]['email']])){
+                        $myPromo->addApprenant( $this->apprenantReposity->findOneBy(['email'=>$users[$p]['email']]));
+                    }else{
+                        $apprenant = new Apprenant();
+                        $apprenant->setNomComplete('null')
+                            ->setUsername($users[$p]['email'])
+                            ->setPassword('passer123')
+                            ->setAdresse('null')
+                            ->setEmail($users[$p]['email'])
+                            ->setGenre('null')
+                            ->setProfil($this->profilRepository->findOneBy(['libelle'=>'Apprenant']))
+                            ->setTelephone('');
+                        $this->manager->persist($apprenant);
+                        $myPromo->addApprenant($apprenant);
+                    }
+                }
+
+            }
+
+        }
+
+        if( $request->files->get('avatar')){
+            $avatar= $request->files->get('avatar');
+            $avatar = fopen($avatar->getRealPath(),'r+');
+            $myPromo->setAvatar($avatar);
+        }
+        foreach ($promoreq as $key=> $valeur){
+            $setter = 'set'. ucfirst(strtolower($key));
+            //dd($setter);
+            if(method_exists(Promo::class, $setter)){
+                if ($key=='dateFinProvisoire'){
+                    try {
+                        $dateFinP = new DateTime($promoreq['dateFinProvisoire']);
+                        $myPromo->setDateFinProvisoire($dateFinP);
+                    } catch (\Exception $e) {
+
+                    }
+                }elseif ($key=='dateFinReel'){
+                    try {
+                        $dateFinR = new DateTime($promoreq['dateFinReel']);
+                        $myPromo->setDateFinReel($dateFinR);
+
+                    } catch (\Exception $e) {
+
+                    }
+                }
+                else{
+                    $myPromo->$setter($valeur);
+
+                }
+
+
+            }
+
+        }
+
+
+        if(isset($promoreq['apprenants'])){
+            $array = explode(',',$promoreq['apprenants']);
+            //dd($array);
+            for ($i=0;$i< count($array)-1;$i++){
+                // dd($this->apprenantReposity->findOneBy(['email'=>$array[$i]]));
+                if( $this->apprenantReposity->findOneBy(['email'=>$array[$i]])){
+
+                    $myPromo->addApprenant( $this->apprenantReposity->findOneBy(['email'=>$array[$i]]));
+                }
+            }
+        }
+
+        $this->manager->persist($myPromo);
+        $this->manager->flush();
+        return new JsonResponse("success",200,[],true);
     }
 
 
@@ -71,7 +159,6 @@ class PromoController extends AbstractController
             $file= $request->files->get('upload');
             $user = $promoService->uploadExcel($file);
            $users = $promoService->toArray($user);
-           //dd($users);
            for ($p =0; $p < count($users); $p++){
                if ($users[$p]['email']){
                    if( $this->apprenantReposity->findOneBy(['email'=>$users[$p]['email']])){
